b07ccc6c841aeb05c422b514ba7ecfe4441fc07e
Inna Cervenkova <inna.cervenkova@gmail.com>
2023-03-26 19:27:31 +0200

    prace s objekty+zakladni styly

---
+++ index.js
@@ -0,0 +1,68 @@
+const flight = {
+  id: '145c01af48dd00002715248e_0',
+  dTimeUTC: '01/26/2021 @ 1:25pm (UTC)',
+  aTimeUTC: '01/26/2021 @ 3:45pm (UTC)',
+  duration: {
+    departure: 8400,
+    return: 0,
+    total: 8400,
+  },
+  fly_duration: '2h 20m',
+  flyFrom: 'PRG',
+  cityFrom: 'Prague',
+  cityCodeFrom: 'PRG',
+  countryFrom: {
+    code: 'CZ',
+    name: 'Czechia',
+  },
+  flyTo: 'BCN',
+  cityTo: 'Barcelona',
+  cityCodeTo: 'BCN',
+  countryTo: {
+    code: 'ES',
+    name: 'Spain',
+  },
+  distance: 1359.54,
+  airline: 'FR',
+  pnr_count: 1,
+  technical_stops: 0,
+  price: 36,
+  bags_price: {
+    1: 34.95,
+    2: 69.91,
+  },
+  baglimit: {
+    hand_width: 20,
+    hand_height: 40,
+    hand_length: 55,
+    hand_weight: 10,
+    hold_width: 81,
+    hold_height: 119,
+    hold_length: 119,
+    hold_dimensions_sum: 319,
+    hold_weight: 20,
+  },
+  availability: {
+    seats: 2,
+  },
+  conversion: {
+    EUR: 36,
+  },
+  quality: 66.66659,
+};
+
+//a)
+document.body.innerHTML += '<p><strong>Startovni zeme:</strong> ' + flight.cityFrom + '<br> <strong>Cilova zeme: </strong>' + flight.cityTo + '</p>';
+
+//b)
+const { baglimit } = flight;
+
+//c)
+document.body.innerHTML += '<p><strong>Maximalni sirka prirucniho zavazadla: </strong>' + baglimit.hand_width + ' cm</p>'
+document.body.innerHTML += '<p><strong>Maximalni vyska prirucniho zavazadla: </strong>' + baglimit.hand_height + ' cm</p>'
+document.body.innerHTML += '<p><strong>Maximalni delka prirucniho zavazadla: </strong>' + baglimit.hand_length + ' cm</p>'
+
+//d)
+const cenaDruhehoZavazadla = Math.ceil(flight.bags_price[2] * flight.conversion.EUR);
+
+document.body.innerHTML += '<p><strong>Cena druheho zavazadla: </strong>' + cenaDruhehoZavazadla + ' Kc </p>';
